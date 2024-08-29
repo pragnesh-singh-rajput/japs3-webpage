7bbe4781ddf8e8e4fca9cef0b0bfa15aeb00b1b2
Pragnesh Singh <singhpragnesh89@gmail.com>
2024-08-29 09:37:08 +0000

    main

---
+++ japs3/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="home-container">
+      <div className="japs3">JAPS3</div>
+      <button className="btn about">About</button>
+      <button className="btn courses">Courses</button>
+      <button className="btn project">Project</button>
+      <div className="school-title">Your Modern Online School</div>
+      <div className="description">
+        JAPS3 is an aggregator of multimedia educational materials from around the world
+      </div>
+      <button className="btn JoinIn">Join In</button>
+      <button className="btn SignIn">Sign In</button>
+      <div className="descript">Trusted by</div>
+      <div className="Box"></div>
+      <div className="learner">500+</div>      
+      <div className="learner2">learners</div>
+    </div>
+  );
+}
+
+export default App;
