dadfb2a3369a3246e20b6971c4497fcef525f924
Hardik Singh <singhhardik531@gmail.com>
2024-09-07 01:07:00 +0530

    changes

--- japs3/src/App.js
+++ japs3/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-//import yellow from './yellowgirl.png';
+import logo from './image/logo.png';
+import yellow from './image/yellowgirl.png';
 
 const App = () => {
   return (
     <div className="home-container">
-        <div className="japs3">JAPS3</div>
+
+      <div className="japs3"><img src={logo} alt='logo'/></div>
         <button className="btn about">About</button>
         <button className="btn courses">Courses</button>
         <button className="btn project">Project</button>
-        <div className="yellow"><img src="./images/yellowgirl.jpeg" alt='yellow'/></div>
+        <div className="yellowrectangle">
+          <div className="yellow"><img src={yellow} alt='yellow'/></div>
+        </div>
         <div className="school-title">Your Modern Online School</div>
         <div className="description">
           JAPS3 is an aggregator of multimedia educational materials from around the world
